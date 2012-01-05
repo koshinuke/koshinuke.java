90aa978bdd7f5a5cc2c74a95fe6231082cbfcb1f
taichi <ryushi@gmail.com>
2012-01-06 04:34:18 +0900

    add test.

--- test/org/koshinuke/service/RepositoryServiceTest.java
+++ test/org/koshinuke/service/RepositoryServiceTest.java
@@ -7,19 +7,48 @@ import org.junit.Before;
 import org.junit.Test;
 import org.koshinuke.model.Repository;
 
-public class RepositoryServiceTest {
+import com.sun.jersey.api.client.WebResource;
+import com.sun.jersey.api.core.ClassNamesResourceConfig;
+import com.sun.jersey.api.core.ResourceConfig;
+import com.sun.jersey.api.json.JSONConfiguration;
+import com.sun.jersey.test.framework.AppDescriptor;
+import com.sun.jersey.test.framework.JerseyTest;
+import com.sun.jersey.test.framework.LowLevelAppDescriptor;
+import com.sun.jersey.test.framework.spi.container.grizzly2.GrizzlyTestContainerFactory;
+
+public class RepositoryServiceTest extends JerseyTest {
 
 	RepositoryService target;
 
+	public RepositoryServiceTest() {
+		super(new GrizzlyTestContainerFactory());
+	}
+
+	@Override
+	protected AppDescriptor configure() {
+		ResourceConfig rc = new ClassNamesResourceConfig(
+				RepositoryService.class);
+		rc.getFeatures().put(JSONConfiguration.FEATURE_POJO_MAPPING, true);
+		return new LowLevelAppDescriptor.Builder(rc).build();
+	}
+
 	@Before
-	public void setUp() {
+	public void setUp() throws Exception {
+		super.setUp();
 		this.target = new RepositoryService();
+	}
+
+	@Test
+	public void testService() throws Exception {
+		WebResource webResource = resource();
+		String responseMsg = webResource.path("a/b/tree/c").get(String.class);
+		assertNotNull(responseMsg);
+
 	}
 
 	@Test
 	public void testName() {
 		Repository r = this.target.name("a", "b");
-
 		assertEquals(r.path, "a");
 		assertEquals(r.name, "b");
 	}

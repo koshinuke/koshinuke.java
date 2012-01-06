adbc13838742d4bbd348e919cc6166882bdf4157
taichi <ryushi@gmail.com>
2012-01-06 20:32:37 +0900

    switch to jetty.

--- src/org/koshinuke/Main.java
+++ src/org/koshinuke/Main.java
@@ -7,13 +7,17 @@ import java.util.Set;
 import javax.ws.rs.core.Application;
 import javax.ws.rs.core.UriBuilder;
 
-import org.glassfish.grizzly.http.server.HttpServer;
+import org.eclipse.jetty.server.Connector;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.nio.SelectChannelConnector;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
 import org.koshinuke.service.RepositoryService;
 
-import com.sun.jersey.api.container.grizzly2.GrizzlyServerFactory;
 import com.sun.jersey.api.core.ApplicationAdapter;
 import com.sun.jersey.api.core.ResourceConfig;
 import com.sun.jersey.api.json.JSONConfiguration;
+import com.sun.jersey.spi.container.servlet.ServletContainer;
 
 public class Main extends Application {
 	
@@ -24,17 +28,31 @@ public class Main extends Application {
 		return classes;
 	}
 	
-	private static URI getBaseURI() {
-		return UriBuilder.fromUri("http://localhost").port(9998).build();
+//	private static URI getBaseURI() {
+//		return UriBuilder.fromUri("http://localhost").port(9998).build();
+//	}
+	
+	protected static Server start()  throws Exception {
+		ResourceConfig rc = new ApplicationAdapter(new Main());
+		rc.getFeatures().put(JSONConfiguration.FEATURE_POJO_MAPPING, true);
+
+		Server server = new Server(9998);
+		server.addConnector(new SelectChannelConnector());
+		ServletHolder holder = new ServletHolder(ServletContainer.class);
+		holder.setInitParameter(ServletContainer.APPLICATION_CONFIG_CLASS, Main.class.getName());
+		holder.setInitParameter(JSONConfiguration.FEATURE_POJO_MAPPING, String.valueOf(true));
+		ServletContextHandler sch = new ServletContextHandler(ServletContextHandler.SESSIONS);
+		sch.addServlet(holder, "/*");
+		server.setHandler(sch);
+		server.start();
+		return server;
 	}
 	
 	public static void main(String[] args) throws Exception {
-		ResourceConfig rc = new ApplicationAdapter(new Main());
-		rc.getFeatures().put(JSONConfiguration.FEATURE_POJO_MAPPING, true);
-		HttpServer httpServer = GrizzlyServerFactory.createHttpServer(getBaseURI(), rc);
+		Server server = start();
 		
 		System.in.read();
-		httpServer.stop();
+		server.stop();
 	}
 
 }
--- src/org/koshinuke/service/RepositoryService.java
+++ src/org/koshinuke/service/RepositoryService.java
@@ -1,10 +1,15 @@
 package org.koshinuke.service;
 
+import java.security.Principal;
+
 import javax.ws.rs.GET;
+import javax.ws.rs.HeaderParam;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.SecurityContext;
 
 import org.koshinuke.model.Repository;
 
@@ -21,6 +26,7 @@ public class RepositoryService {
 	@GET
 	@Path("/{path}/{project}")
 	@Produces(MediaType.APPLICATION_JSON)
+	//@HeaderParam("X-Requested-With")
 	public Repository name(@PathParam("path") String path,
 			@PathParam("project") String project) {
 		Repository r = new Repository();
@@ -33,10 +39,18 @@ public class RepositoryService {
 
 	@GET
 	@Path("/{path}/{project}/tree/{branch}")
+	//@HeaderParam("X-Requested-With")
 	public String tree(@PathParam("path") String path,
 			@PathParam("project") String project,
 			@PathParam("branch") String branch) {
 		
 		return path + "--" + project + "--" + branch;
+	}
+	
+	//@GET
+	public String login(@Context SecurityContext sc) {
+		Principal p =  sc.getUserPrincipal();
+		System.out.println(p.getName());
+		return "";
 	}
 }

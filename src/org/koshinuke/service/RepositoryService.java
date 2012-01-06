5604b50defada06b4a2b0de5a443e502a8346a83
taichi <ryushi@gmail.com>
2012-01-07 02:38:26 +0900

    org imports.

--- src/org/koshinuke/service/RepositoryService.java
+++ src/org/koshinuke/service/RepositoryService.java
@@ -3,7 +3,6 @@ package org.koshinuke.service;
 import java.security.Principal;
 
 import javax.ws.rs.GET;
-import javax.ws.rs.HeaderParam;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -18,7 +17,7 @@ import com.sun.jersey.spi.resource.Singleton;
 @Singleton
 @Path("")
 public class RepositoryService {
-	
+
 	public RepositoryService() {
 		System.out.println("new!!");
 	}
@@ -26,7 +25,7 @@ public class RepositoryService {
 	@GET
 	@Path("/{path}/{project}")
 	@Produces(MediaType.APPLICATION_JSON)
-	//@HeaderParam("X-Requested-With")
+	// @HeaderParam("X-Requested-With")
 	public Repository name(@PathParam("path") String path,
 			@PathParam("project") String project) {
 		Repository r = new Repository();
@@ -39,17 +38,17 @@ public class RepositoryService {
 
 	@GET
 	@Path("/{path}/{project}/tree/{branch}")
-	//@HeaderParam("X-Requested-With")
+	// @HeaderParam("X-Requested-With")
 	public String tree(@PathParam("path") String path,
 			@PathParam("project") String project,
 			@PathParam("branch") String branch) {
-		
+
 		return path + "--" + project + "--" + branch;
 	}
-	
-	//@GET
+
+	// @GET
 	public String login(@Context SecurityContext sc) {
-		Principal p =  sc.getUserPrincipal();
+		Principal p = sc.getUserPrincipal();
 		System.out.println(p.getName());
 		return "";
 	}
